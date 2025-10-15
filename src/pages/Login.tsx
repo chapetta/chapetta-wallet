891024c0d0b99bf68840fd4aa9dae243c37618a4
chapetta <cha.petta@hotmail.com>
2025-10-15 10:49:09 -0300

    fazendo primeira parte da pagina do login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,104 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  email: z.email({ message: "E-mail inválido" }),
+  password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
+});
+
+type LoginData = z.infer<typeof loginSchema>;
+
 export const Login = () => {
+  const form = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: LoginData) => {
+    console.log("Login", data);
+  };
+
   return (
-    <form>
-      
-    </form>
-  )
+    <div className="h-screen flex items-center justify-center bg-emerald-500 ">
+      <Card className="w-[525px] shadow-xl bg-white h-auto rounded-[10px]">
+        <CardHeader className="text-center">
+          <CardTitle className="text-3xl font-bold">
+            Chaps <span className="text-emerald-400">Wallet</span>{" "}
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col gap-4 mt-6 px-6"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>E-mail:</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        {...field}
+                        placeholder="exemple@hotmail.com"
+                      />
+                    </FormControl>
+                    <FormMessage className="text-red-600" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password:</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Digite sua senha"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-red-600" />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                variant="ghost"
+                size="lg"
+                className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold my-5"
+              >
+                Entrar
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
