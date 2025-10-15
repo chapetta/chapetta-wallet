8ebc5301169a8c887121e5ebeaec5d1a86bfad35
chapetta <cha.petta@hotmail.com>
2025-10-15 12:24:42 -0300

    criando a pagina wallet e a rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
+import { Wallet } from "@/pages/Wallet";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/wallet" element={<Wallet />} />
     </Routes>
   );
 };
